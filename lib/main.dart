2377f27b79a5d0aecef91d13fec18757c8bbc6a8
whes1015 <whes1015@gmail.com>
2023-09-29 02:52:07 +0800

    fix: fcm

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:dpip/view/init.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 @pragma('vm:entry-point')
 Future<void> _BackgroundHandler(RemoteMessage message) async {
@@ -26,6 +25,7 @@ void main() async{
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
 
   });
+  print(await messaging.getToken());
   runApp(const MyApp());
 }
 
