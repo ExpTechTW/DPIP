fc6cdb48d690ac4e4c0f7302f5962895fd5ab3d4
whes1015 <whes1015@gmail.com>
2023-10-04 02:18:33 +0800

    fix: history

--- lib/view/history.dart
+++ lib/view/history.dart
@@ -25,7 +25,7 @@ class _HistoryPage extends State<HistoryPage> {
   void render() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     data ??= await get(
-        "https://exptech.com.tw/api/v1/dpip/history?city=${prefs.getString('loc-city')}&town=${prefs.getString('loc-town')}");
+        "https://exptech.com.tw/api/v1/dpip/history?city=${prefs.getString('loc-city')??"臺南市"}&town=${prefs.getString('loc-town')??"歸仁區"}");
     _List_children = <Widget>[];
     if (data == null || data == false) {
       _List_children.add(const Column(
@@ -44,117 +44,98 @@ class _HistoryPage extends State<HistoryPage> {
       ));
     } else {
       if (_page == 0) {
-        if (prefs.getString('loc-town') == null) {
-          _List_children.add(const Column(
+        _List_children.add(Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 0, 0),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "服務區域外",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w100,
-                    color: Colors.white),
-              ),
-              Text(
-                "無法取得相關資訊 可能是因為尚未設定所在地位置",
-                style: TextStyle(fontSize: 16, color: Colors.white),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text(
+                    prefs.getString("loc-city") ?? "臺南市",
+                    style: const TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    prefs.getString("loc-town") ?? "歸仁區",
+                    style: const TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.grey),
+                  ),
+                ],
               )
             ],
-          ));
-        } else {
-          _List_children.add(Padding(
-            padding: const EdgeInsets.fromLTRB(10, 5, 0, 0),
+          ),
+        ));
+        if (data["loc"].length == 0) {
+          _List_children.add(const Padding(
+            padding: EdgeInsets.all(10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    Text(
-                      prefs.getString("loc-city") ?? "臺南市",
-                      style: const TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.white),
-                    ),
-                    const SizedBox(width: 10),
-                    Text(
-                      prefs.getString("loc-town") ?? "歸仁區",
-                      style: const TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.grey),
-                    ),
-                  ],
+                SizedBox(width: double.infinity),
+                Text(
+                  "過去 72小時 暫無防災資訊",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
                 )
               ],
             ),
           ));
-          if (data["loc"].length == 0) {
-            _List_children.add(const Padding(
-              padding: EdgeInsets.all(10),
+        } else {
+          for (var i = 0; i < data["loc"].length; i++) {
+            DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(
+                    data["loc"][i]["time"],
+                    isUtc: true)
+                .add(const Duration(hours: 8));
+            String formattedDate =
+                '${dateTime.year}年${formatNumber(dateTime.month)}月${formatNumber(dateTime.day)}日 ${formatNumber(dateTime.hour)}:${formatNumber(dateTime.minute)} 發布';
+            _List_children.add(Padding(
+              padding: const EdgeInsets.all(10),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(width: double.infinity),
+                  const SizedBox(width: double.infinity),
+                  Row(
+                    children: [
+                      Icon(
+                        (data["loc"][i]["type"] == 2)
+                            ? Icons.warning_amber_outlined
+                            : (data["loc"][i]["type"] == 1)
+                                ? Icons.doorbell_outlined
+                                : Icons.speaker_notes_outlined,
+                        color: (data["loc"][i]["type"] == 2)
+                            ? Colors.red
+                            : (data["loc"][i]["type"] == 1)
+                                ? Colors.amber
+                                : Colors.white,
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        data["loc"][i]["title"],
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
                   Text(
-                    "過去 72小時 暫無防災資訊",
-                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    formattedDate,
+                    style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                  Text(
+                    data["loc"][i]["body"],
+                    style: const TextStyle(fontSize: 16, color: Colors.white),
                   )
                 ],
               ),
             ));
-          } else {
-            for (var i = 0; i < data["loc"].length; i++) {
-              DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(
-                      data["loc"][i]["time"],
-                      isUtc: true)
-                  .add(const Duration(hours: 8));
-              String formattedDate =
-                  '${dateTime.year}年${formatNumber(dateTime.month)}月${formatNumber(dateTime.day)}日 ${formatNumber(dateTime.hour)}:${formatNumber(dateTime.minute)} 發布';
-              _List_children.add(Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(width: double.infinity),
-                    Row(
-                      children: [
-                        Icon(
-                          (data["loc"][i]["type"] == 2)
-                              ? Icons.warning_amber_outlined
-                              : (data["loc"][i]["type"] == 1)
-                                  ? Icons.doorbell_outlined
-                                  : Icons.speaker_notes_outlined,
-                          color: (data["loc"][i]["type"] == 2)
-                              ? Colors.red
-                              : (data["loc"][i]["type"] == 1)
-                                  ? Colors.amber
-                                  : Colors.white,
-                        ),
-                        const SizedBox(width: 5),
-                        Text(
-                          data["loc"][i]["title"],
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      formattedDate,
-                      style: const TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                    Text(
-                      data["loc"][i]["body"],
-                      style: const TextStyle(fontSize: 16, color: Colors.white),
-                    )
-                  ],
-                ),
-              ));
-            }
           }
         }
       } else {
