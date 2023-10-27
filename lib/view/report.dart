94fbc7c42cf3306e18f1df8f42f8626cdf629e33
Im-Tim-mI <rr901037@gmail.com>
2023-10-28 03:35:54 +0800

    chore: Add drop-down list

--- lib/view/report.dart
+++ lib/view/report.dart
@@ -10,46 +10,84 @@ class ReportPage extends StatefulWidget {
 }
 
 class _ReportPage extends State<ReportPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
               children: [
-                Text(
-                  "地震報告",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.white,
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "地震報告",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 450,
+                  child: FlutterMap(
+                    // key: ValueKey(_page),
+                    // mapController: mapController,
+                    options: MapOptions(
+                      center: const LatLng(23.4, 120.1),
+                      zoom: 7,
+                      minZoom: 7,
+                      maxZoom: 9,
+                      interactiveFlags:
+                      InteractiveFlag.all - InteractiveFlag.rotate,
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                        "https://api.mapbox.com/styles/v1/whes1015/clne7f5m500jd01re1psi1cd2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoid2hlczEwMTUiLCJhIjoiY2xuZTRhbmhxMGIzczJtazN5Mzg0M2JscCJ9.BHkuZTYbP7Bg1U9SfLE-Cg",
+                      ),
+                    ],
                   ),
-                )
+                ),
               ],
             ),
-            SizedBox(
-              height: 450,
-              child: FlutterMap(
-                // key: ValueKey(_page),
-                // mapController: mapController,
-                options: MapOptions(
-                  center: const LatLng(23.4, 120.1),
-                  zoom: 7,
-                  minZoom: 7,
-                  maxZoom: 9,
-                  interactiveFlags:
-                      InteractiveFlag.all - InteractiveFlag.rotate,
-                ),
-                children: [
-                  TileLayer(
-                    urlTemplate:
-                        "https://api.mapbox.com/styles/v1/whes1015/clne7f5m500jd01re1psi1cd2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoid2hlczEwMTUiLCJhIjoiY2xuZTRhbmhxMGIzczJtazN5Mzg0M2JscCJ9.BHkuZTYbP7Bg1U9SfLE-Cg",
+            DraggableScrollableSheet(
+              initialChildSize: 0.3,
+              minChildSize: 0.2,
+              maxChildSize: 1.0,
+              builder:
+                  (BuildContext context, ScrollController scrollController) {
+                return Container(
+                  color: Colors.black.withOpacity(0.8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: ScrollConfiguration(
+                      behavior: ScrollConfiguration.of(context)
+                          .copyWith(overscroll: false),
+                      child: ListView(
+                        controller: scrollController,
+                        children: [
+                          Center(
+                            child: Text(
+                              "顯著有感地震",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w800,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ],
-              ),
+                );
+              },
             ),
           ],
         ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,7 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
