024e550791902199890d89c1c0c18c6bfddc7b80
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-04-16 10:55:24 +0800

    fix: capsule text color

--- lib/widget/report/intensity_capsule.dart
+++ lib/widget/report/intensity_capsule.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dpip/core/utils.dart';
 import 'package:dpip/util/extension.dart';
 import 'package:dpip/util/intensity_color.dart';
@@ -16,46 +18,96 @@ class IntensityCapsule extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-          color: context.colors.intensity(intensity),
+    if (Platform.isIOS) {
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: context.colors.intensity(intensity),
+          ),
+          color: context.colors.intensity(intensity).withOpacity(0.08),
         ),
-        color: context.colors.intensity(intensity).withOpacity(0.08),
-      ),
-      margin: EdgeInsets.zero,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            height: 30,
-            width: 30,
-            decoration: BoxDecoration(
-              color: context.colors.intensity(intensity),
-              borderRadius: BorderRadius.circular(16),
+        margin: EdgeInsets.zero,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: context.colors.intensity(intensity),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: Text(
+                  intensityToNumberString(intensity),
+                  style: TextStyle(
+                      color: context.colors.onIntensity(intensity),
+                      height: 1,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
-            child: Center(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(6, 6, 12, 6),
               child: Text(
-                intensityToNumberString(intensity),
+                townName,
                 style: TextStyle(
-                    color: context.colors.onIntensity(intensity), height: 1, fontSize: 16, fontWeight: FontWeight.bold),
+                  color: CupertinoColors.label.resolveFrom(context),
+                  height: 1,
+                  fontSize: 14,
+                ),
               ),
             ),
+          ],
+        ),
+      );
+    } else {
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: context.colors.intensity(intensity),
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(6, 6, 12, 6),
-            child: Text(
-              townName,
-              style: TextStyle(
-                color: context.colors.onSurfaceVariant.harmonizeWith(context.colors.intensity(intensity)),
-                height: 1,
-                fontSize: 14,
+          color: context.colors.intensity(intensity).withOpacity(0.08),
+        ),
+        margin: EdgeInsets.zero,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: context.colors.intensity(intensity),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: Text(
+                  intensityToNumberString(intensity),
+                  style: TextStyle(
+                      color: context.colors.onIntensity(intensity),
+                      height: 1,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-    );
+            Padding(
+              padding: const EdgeInsets.fromLTRB(6, 6, 12, 6),
+              child: Text(
+                townName,
+                style: TextStyle(
+                  color: context.colors.onSurfaceVariant.harmonizeWith(context.colors.intensity(intensity)),
+                  height: 1,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
