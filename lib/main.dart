6ea7d8860e60241c99456adb78c074cadd61ce02
lowrt <nineder197789@gmail.com>
2024-05-09 21:08:09 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,50 +25,6 @@ const String darwinNotificationCategoryText = 'textCategory';
 const String navigationActionId = 'id_3';
 const String darwinNotificationCategoryPlain = 'plainCategory';
 
-final List<DarwinNotificationCategory> darwinNotificationCategories = <DarwinNotificationCategory>[
-  DarwinNotificationCategory(
-    darwinNotificationCategoryText,
-    actions: <DarwinNotificationAction>[
-      DarwinNotificationAction.text(
-        'text_1',
-        'Action 1',
-        buttonTitle: 'Send',
-        placeholder: 'Placeholder',
-      ),
-    ],
-  ),
-  DarwinNotificationCategory(
-    darwinNotificationCategoryPlain,
-    actions: <DarwinNotificationAction>[
-      DarwinNotificationAction.plain('id_1', 'Action 1'),
-      DarwinNotificationAction.plain(
-        'id_2',
-        'Action 2 (destructive)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.destructive,
-        },
-      ),
-      DarwinNotificationAction.plain(
-        navigationActionId,
-        'Action 3 (foreground)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.foreground,
-        },
-      ),
-      DarwinNotificationAction.plain(
-        'id_4',
-        'Action 4 (auth required)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.authenticationRequired,
-        },
-      ),
-    ],
-    options: <DarwinNotificationCategoryOption>{
-      DarwinNotificationCategoryOption.hiddenPreviewShowTitle,
-    },
-  )
-];
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Global.init();
@@ -99,7 +55,6 @@ void main() async {
         ),
       );
     },
-    notificationCategories: darwinNotificationCategories,
   );
   final initializationSettings = InitializationSettings(
     android: const AndroidInitializationSettings('ic_launcher'),
