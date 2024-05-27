dd560a42908821ad2bd8a9cdbffd82374b89af76
lowrt <nineder197789@gmail.com>
2024-05-27 09:59:36 +0800

    fix: Android settings

--- lib/view/setting/location_utils.dart
+++ lib/view/setting/location_utils.dart
@@ -1,21 +1,22 @@
 import 'dart:io';
 
-
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> openLocationSettings() async {
-  const urlAndroid = 'package:com.android.settings';
+  const urlAndroid = 'intent://#Intent;action=android.settings.LOCATION_SOURCE_SETTINGS;end';
   const urlIOS = 'app-settings:';
 
   if (Platform.isAndroid) {
-    if (await canLaunchUrl(Uri.parse(urlAndroid))) {
-      await launchUrl(Uri.parse(urlAndroid));
+    final uriAndroid = Uri.parse(urlAndroid);
+    if (await canLaunchUrl(uriAndroid)) {
+      await launchUrl(uriAndroid, mode: LaunchMode.externalApplication);
     } else {
       throw 'Could not launch $urlAndroid';
     }
   } else if (Platform.isIOS) {
-    if (await canLaunchUrl(Uri.parse(urlIOS))) {
-      await launchUrl(Uri.parse(urlIOS));
+    final uriIOS = Uri.parse(urlIOS);
+    if (await canLaunchUrl(uriIOS)) {
+      await launchUrl(uriIOS);
     } else {
       throw 'Could not launch $urlIOS';
     }
