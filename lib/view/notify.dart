858bee5d0b39df491af0df80fa28d12269abedbb
charlie112787 <sscharlie1127@gmail.com>
2024-05-31 23:08:42 +0800

    feat/音效播放外觀

--- lib/view/notify.dart
+++ lib/view/notify.dart
@@ -20,9 +20,40 @@ class NotifyPage extends StatelessWidget {
           title: const Text("音效測試"),
         ),
         body: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0x660063C6),
+                    Color(0xFF0063C6),
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(90)),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // 靠兩側(其實我看不太懂
+                children: [
+                  Text(
+                    '強震即時警報 (EEW) ',
+                    style: TextStyle(fontSize: 21,fontWeight: FontWeight.bold),
+                  ),
+                  Icon(
+                    Icons.play_circle,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       );
     }
   }
 }
-
