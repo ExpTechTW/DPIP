90097db760ef231bd9f3eff7fdbc553c5fb2b452
Yoyochou0901 <yoyochou0901@gmail.com>
2024-06-09 18:55:31 +0800

    Update home_page.dart

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -23,7 +23,7 @@ class HomePage extends StatefulWidget {
 
 class Areas {
   static List<String> getOptions(currentArea) {
-    return [currentArea, '花蓮縣 萬榮鄉', '臺北市 中正區']; // 鄉鎮列表
+    return [currentArea, '花蓮縣 萬榮鄉', '宜蘭縣 南澳鄉']; // 鄉鎮列表
   }
 }
 
