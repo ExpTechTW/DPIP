447d29dd3b711cc6b260a036ff422acf9037193b
lowrt <nineder197789@gmail.com>
2024-06-09 21:36:28 +0800

    feat: 增加ios並更換音效(待增加)

--- lib/view/notify.dart
+++ lib/view/notify.dart
@@ -22,17 +22,17 @@ class NotifyPage extends StatelessWidget {
         ),
         child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
+            padding: const EdgeInsets.all(16),
             child: SingleChildScrollView(
               child: Column(
                 children: [
                   Container(
                     margin: const EdgeInsets.only(bottom: 10),
-                    padding: const EdgeInsets.all(20),
+                    padding: const EdgeInsets.all(16),
                     decoration: const BoxDecoration(
                       gradient: LinearGradient(
                         colors: [
-                          Color(0x660063C6),
+                          Color(0x330063C6),
                           Color(0xFF0063C6),
                         ],
                         begin: Alignment.centerLeft,
@@ -44,9 +44,11 @@ class NotifyPage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         const Text(
-                          '強震即時警報 (EEW) ',
+                          '強震即時警報(警報)',
                           style: TextStyle(
-                              fontSize: 21, fontWeight: FontWeight.bold),
+                              fontSize: 21,
+                              fontWeight: FontWeight.bold,
+                          ),
                         ),
                         CupertinoButton(
                           onPressed: () {
@@ -64,7 +66,197 @@ class NotifyPage extends StatelessWidget {
                   ),
                   Container(
                     margin: const EdgeInsets.only(bottom: 10),
-                    padding: const EdgeInsets.all(20),
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0x330063C6),
+                          Color(0xFF0063C6),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(90)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '地震速報(注意)',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        CupertinoButton(
+                          onPressed: () {
+                            playSound("eew_warn.wav");
+                          },
+                          padding: EdgeInsets.zero,
+                          child: const Icon(
+                            CupertinoIcons.play_circle_fill,
+                            color: CupertinoColors.white,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0x33F8E495),
+                          Color(0xFFF8E495),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(90)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '震度速報',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        CupertinoButton(
+                          onPressed: () {
+                            playSound("warn.wav");
+                          },
+                          padding: EdgeInsets.zero,
+                          child: const Icon(
+                            CupertinoIcons.play_circle_fill,
+                            color: CupertinoColors.white,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0x33F8E495),
+                          Color(0xFFF8E495),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(90)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '強震監視器',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        CupertinoButton(
+                          onPressed: () {
+                            playSound("warn.wav");
+                          },
+                          padding: EdgeInsets.zero,
+                          child: const Icon(
+                            CupertinoIcons.play_circle_fill,
+                            color: CupertinoColors.white,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0x33F8E495),
+                          Color(0xFFF8E495),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(90)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '地震報告',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        CupertinoButton(
+                          onPressed: () {
+                            playSound("warn.wav");
+                          },
+                          padding: EdgeInsets.zero,
+                          child: const Icon(
+                            CupertinoIcons.play_circle_fill,
+                            color: CupertinoColors.white,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0x1AFD9800),
+                          Color(0xFFFD9800),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(90)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '大雷雨及時訊息',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        CupertinoButton(
+                          onPressed: () {
+                            playSound("warn.wav");
+                          },
+                          padding: EdgeInsets.zero,
+                          child: const Icon(
+                            CupertinoIcons.play_circle_fill,
+                            color: CupertinoColors.white,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.all(16),
                     decoration: const BoxDecoration(
                       gradient: LinearGradient(
                         colors: [
@@ -87,7 +279,9 @@ class NotifyPage extends StatelessWidget {
                           ),
                         ),
                         CupertinoButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            playSound("warn.wav");
+                          },
                           padding: EdgeInsets.zero,
                           child: const Icon(
                             CupertinoIcons.play_circle_fill,
@@ -175,7 +369,7 @@ class NotifyPage extends StatelessWidget {
                   ),
                   IconButton(
                     onPressed: () {
-                      playSound("eew_alert.wav");
+                      playSound("eew_warn.wav");
                     },
                     icon: const Icon(
                       Icons.play_circle,
@@ -212,7 +406,7 @@ class NotifyPage extends StatelessWidget {
                   ),
                   IconButton(
                     onPressed: () {
-                      playSound("eew_alert.wav");
+                      playSound("warn.wav");
                     },
                     icon: const Icon(
                       Icons.play_circle,
@@ -249,7 +443,7 @@ class NotifyPage extends StatelessWidget {
                   ),
                   IconButton(
                     onPressed: () {
-                      playSound("eew_alert.wav");
+                      playSound("warn.wav");
                     },
                     icon: const Icon(
                       Icons.play_circle,
@@ -286,7 +480,7 @@ class NotifyPage extends StatelessWidget {
                   ),
                   IconButton(
                     onPressed: () {
-                      playSound("eew_alert.wav");
+                      playSound("warn.wav");
                     },
                     icon: const Icon(
                       Icons.play_circle,
@@ -323,7 +517,7 @@ class NotifyPage extends StatelessWidget {
                   ),
                   IconButton(
                     onPressed: () {
-                      playSound("eew_alert.wav");
+                      playSound("warn.wav");
                     },
                     icon: const Icon(
                       Icons.play_circle,
