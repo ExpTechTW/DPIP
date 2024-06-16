11c90bd34189f8f87407fc256c78da86c643d73c
Yoyochou0901 <yoyochou0901@gmail.com>
2024-06-16 13:43:43 +0800

    Create welcome.dart

---
+++ lib/view/welcome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
