2d81a8bca44b5e94e7becebe697b6ca6257797a9
lowrt <nineder197789@gmail.com>
2024-06-20 13:00:41 +0800

    fix: 不跳轉設定

--- lib/view/setting/location_utils.dart
+++ lib/view/setting/location_utils.dart
@@ -1,38 +1,15 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:geolocator/geolocator.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 Future<bool> openLocationSettings() async {
-  const urlIOS = 'app-settings:';
+  LocationPermission permission = await Geolocator.checkPermission();
 
-  if (Platform.isAndroid) {
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission != LocationPermission.always) {
-      permission = await Geolocator.requestPermission();
-      if (permission != LocationPermission.always) {
-        permission = await Geolocator.requestPermission();
-        if (permission != LocationPermission.always) {
-          return false;
-        } else {
-          return true;
-        }
-      } else {
-        return true;
-      }
-    } else {
-      return true;
-    }
-  } else if (Platform.isIOS) {
-    final uriIOS = Uri.parse(urlIOS);
-    if (await canLaunchUrl(uriIOS)) {
-      await launchUrl(uriIOS);
-      return true;
-    } else {
-      throw 'Could not launch $urlIOS';
+  if (permission != LocationPermission.always && permission != LocationPermission.whileInUse) {
+    permission = await Geolocator.requestPermission();
+    if (permission != LocationPermission.always && permission != LocationPermission.whileInUse) {
+      return false;
     }
   }
-
-  return false;
+  return true;
 }
