eb9e5a008c10b9561c24d0dfd2f1680a677ace03
Yoyochou0901 <yoyochou0901@gmail.com>
2024-06-20 20:47:08 +0800

    feat: 畫面淡出

--- lib/view/welcome/welcome.dart
+++ lib/view/welcome/welcome.dart
@@ -8,6 +8,18 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -26,74 +38,77 @@ class _WelcomePageState extends State<WelcomePage> {
                     ],
                   ),
                 ),
-                child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Stack(children: [
-                      Align(
-                        alignment: Alignment.center,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.asset(
-                                'assets/app_icon.png',
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            const Column(
+                child: AnimatedOpacity(
+                    opacity: _opacity,
+                    duration: const Duration(seconds: 1),
+                    child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Stack(children: [
+                          Align(
+                            alignment: Alignment.center,
+                            child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  "DPIP",
-                                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.asset(
+                                    'assets/app_icon.png',
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
-                                Text(
-                                  "防災資訊整合平台",
-                                  style: TextStyle(fontSize: 16),
+                                const SizedBox(
+                                  width: 20,
                                 ),
-                                SizedBox(
-                                  height: 10,
+                                const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "DPIP",
+                                      style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      "防災資訊整合平台",
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            gradient: const LinearGradient(
-                              colors: [Color(0xFF009E8B), Color(0xFF203864)],
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                            ),
                           ),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .push(MaterialPageRoute(builder: (context) => const WelcomeNotePage()));
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.transparent,
-                              elevation: 0,
-                            ),
-                            child: const Text(
-                              "下一步",
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                gradient: const LinearGradient(
+                                  colors: [Color(0xFF009E8B), Color(0xFF203864)],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .push(MaterialPageRoute(builder: (context) => const WelcomeNotePage()));
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.transparent,
+                                  elevation: 0,
+                                ),
+                                child: const Text(
+                                  "下一步",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    ])))));
+                        ]))))));
   }
 }
