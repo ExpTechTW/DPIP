03cc77466dc8a518c3f72971f58cad79ea21a73d
Yoyochou0901 <yoyochou0901@gmail.com>
2024-06-20 18:45:03 +0800

    feat: 顯示Logo

--- lib/view/welcome/welcome.dart
+++ lib/view/welcome/welcome.dart
@@ -12,42 +12,64 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            appBar: AppBar(
-              title: const Text("歡迎"),
-            ),
-            body: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                  const Text("內文"),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        gradient: const LinearGradient(
-                          colors: [Color(0xFF009E8B), Color(0xFF203864)],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                      ),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const WelcomeNotePage()));
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          elevation: 0,
-                        ),
-                        child: const Text(
-                          "下一步",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
+            // appBar: AppBar(
+            //   title: const Text("歡迎"),
+            // ),
+            body: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color.lerp(const Color(0xFF009E8B), Colors.black, 0.5)!,
+                      Color.lerp(const Color(0xFF203864), Colors.black, 0.5)!
+                    ],
+                  ),
+                ),
+                child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Stack(children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0.0), // 設定圓角半徑
+                          child: Image.asset(
+                            'assets/app_icon.png',
+                            width: 100,
+                            height: 100,
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                ]))));
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            gradient: const LinearGradient(
+                              colors: [Color(0xFF009E8B), Color(0xFF203864)],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (context) => const WelcomeNotePage()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              elevation: 0,
+                            ),
+                            child: const Text(
+                              "下一步",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ])))));
   }
 }
