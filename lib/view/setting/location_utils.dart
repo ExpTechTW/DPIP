ea2e3c1f62eba17e5b7fb674a8ebb0b42290f11b
lowrt <nineder197789@gmail.com>
2024-06-25 23:25:09 +0800

    Update location_utils.dart

--- lib/view/setting/location_utils.dart
+++ lib/view/setting/location_utils.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Future<bool> openLocationSettings() async {
-  LocationPermission permission = await Geolocator.checkPermission();
+  if (Platform.isAndroid) {
+    LocationPermission permission = await Geolocator.checkPermission();
 
-  if (permission != LocationPermission.always) {
-    await Geolocator.requestPermission();
-    permission = await Geolocator.checkPermission();
     if (permission != LocationPermission.always) {
       await Geolocator.requestPermission();
       permission = await Geolocator.checkPermission();
@@ -15,7 +15,13 @@ Future<bool> openLocationSettings() async {
         await Geolocator.requestPermission();
         permission = await Geolocator.checkPermission();
         if (permission != LocationPermission.always) {
-          return false;
+          await Geolocator.requestPermission();
+          permission = await Geolocator.checkPermission();
+          if (permission != LocationPermission.always) {
+            return false;
+          } else {
+            return true;
+          }
         } else {
           return true;
         }
@@ -25,7 +31,13 @@ Future<bool> openLocationSettings() async {
     } else {
       return true;
     }
-  } else {
-    return true;
+  } else if (Platform.isIOS) {
+    const urlIOS = 'app-settings:';
+    final uriIOS = Uri.parse(urlIOS);
+    if (await canLaunchUrl(uriIOS)) {
+      await launchUrl(uriIOS);
+      return true;
+    }
   }
+  return false;
 }
