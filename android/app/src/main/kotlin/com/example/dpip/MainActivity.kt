1df2fd718f6b7afeebe490e99b2d64fc00f8b0db
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-06-27 18:34:23 +0800

    feat: BootBroadcastReceiver

--- android/app/src/main/kotlin/com/example/dpip/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dpip
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/exptech/dpip/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exptech.dpip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
