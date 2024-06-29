4e59bbf1b97ab17be56d732b0faef82b84aad159
lowrt <nineder197789@gmail.com>
2024-06-29 09:29:29 +0800

    fix: ios
    暫時無定位權限

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,28 @@
 import UIKit
+import CoreLocation
 import Flutter
 import Firebase
 
+class YourLocationManagerClass: NSObject, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager?
+    
+    func requestLocationPermission() {
+            locationManager = CLLocationManager()
+        }
+}
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    var locationManager: YourLocationManagerClass?
+    
+    override func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+      FirebaseApp.configure()
+      GeneratedPluginRegistrant.register(with: self)
+      locationManager = YourLocationManagerClass()
+      locationManager?.requestLocationPermission()
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import connectivity_plus
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
+import geolocator_apple
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
