1bcc270525bd76fabd2cc890652ec7016e88b240
whes1015 <whes1015@gmail.com>
2024-06-30 22:04:13 +0800

    fix: home page map zoom

--- lib/app/page/home/home.dart
+++ lib/app/page/home/home.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
 
         },
         minMaxZoomPreference: const MinMaxZoomPreference(
-          0,
+          4,
           10,
         ),
         initialCameraPosition: const CameraPosition(
