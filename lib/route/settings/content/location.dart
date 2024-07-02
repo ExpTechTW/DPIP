cf1488adf06d41e200bcbc1168a2db510cfb31dc
夜夜 <vcde0000@gmail.com>
2024-07-03 03:16:56 +0800

    fix: location

--- lib/route/settings/content/location.dart
+++ lib/route/settings/content/location.dart
@@ -94,8 +94,10 @@ class _SettingsLocationViewState extends State<SettingsLocationView> {
       status = await Permission.location.status;
     } else if (value == 1) {
       status = await Permission.location.request();
-    } else {
+    } else if (value == 2) {
       status = await Permission.locationAlways.request();
+    } else {
+      status = await Permission.location.status;
     }
     if (!mounted) return false;
 
@@ -108,7 +110,9 @@ class _SettingsLocationViewState extends State<SettingsLocationView> {
           return AlertDialog(
             icon: const Icon(Symbols.error),
             title: const Text("無法取得位置權限"),
-            content: value == 0 ? Text(
+            content: value == 0 ? const Text(
+              "自動定位功能需要您允許 DPIP 使用位置權限才能正常運作。",
+            ) : value == 1 ? Text(
               "自動定位功能需要您允許 DPIP 使用位置權限才能正常運作。${status.isPermanentlyDenied ? "請您到應用程式設定中找到並允許「位置」權限後再試一次。" : ""}",
             ) : const Text("為了獲得更好的自動定位體驗，您需要將位置權限提升至「一律允許」以讓 DPIP 在背景自動設定所在地資訊。"),
             actionsAlignment: MainAxisAlignment.spaceBetween,
@@ -119,7 +123,7 @@ class _SettingsLocationViewState extends State<SettingsLocationView> {
                   Navigator.pop(context);
                 },
               ),
-              status.isPermanentlyDenied && value == 2
+              !status.isGranted && value == 2
                   ? FilledButton(
                       child: const Text("設定"),
                       onPressed: () {
