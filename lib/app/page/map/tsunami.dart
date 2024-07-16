a336661bf19842bffe16288f760f7e9c89a1ce38
Yoyochou0901 <yoyochou0901@gmail.com>
2024-07-16 23:39:49 +0800

    feat: 海嘯警報資訊

--- lib/app/page/map/tsunami.dart
+++ lib/app/page/map/tsunami.dart
@@ -3,14 +3,41 @@ import 'package:dpip/util/extension/build_context.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TsunamiMap extends StatelessWidget {
-  const TsunamiMap({super.key});
+import '../../../model/tsunami/tsunami.dart';
 
+class TsunamiMap extends StatefulWidget {
+  const TsunamiMap({super.key});
+  @override
+  State<StatefulWidget> createState() => _TsunamiMapState();
+}
+
+class _TsunamiMapState extends State<TsunamiMap> {
+  Tsunami? tsunami;
+  String tsunamiStatus = "";
+  bool refreshingTsunami = true;
   refreshTsunami() async {
+    refreshingTsunami = true;
     var idList = await ExpTech().getTsunamiList();
-    var id = idList[0];
-    var tsunami = ExpTech().getTsunami(id);
+    var id = "";
+    if (idList.isNotEmpty) {
+      id = idList[0];
+      tsunami = await ExpTech().getTsunami(id);
+      (tsunami?.status == 0)
+          ? tsunamiStatus = "發布"
+          : (tsunami?.status == 1)
+              ? tsunamiStatus = "更新"
+              : tsunamiStatus = "解除";
+    }
+    setState(() {
+      refreshingTsunami = false;
+    });
     return tsunami;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    refreshTsunami();
   }
 
   @override
@@ -42,28 +69,40 @@ class TsunamiMap extends StatelessWidget {
                 ),
                 Padding(
                   padding: const EdgeInsets.only(left: 20, right: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "近期無海嘯資訊",
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 2,
-                          color: context.colors.onSurface,
+                  child: refreshingTsunami == true
+                      ? Container()
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tsunami == null ? "近期無海嘯資訊" : "海嘯警報",
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2,
+                                color: context.colors.onSurface,
+                              ),
+                            ),
+                            tsunami != null
+                                ? Text(
+                                    "${tsunami?.id}號 第${tsunami?.serial}報",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      letterSpacing: 1,
+                                      color: context.colors.onSurface,
+                                    ),
+                                  )
+                                : Container(),
+                            Text(
+                              "2024/07/01 00:00 $tsunamiStatus",
+                              style: TextStyle(
+                                fontSize: 14,
+                                letterSpacing: 1,
+                                color: context.colors.onSurfaceVariant,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Text(
-                        "2024/07/01 00:00 更新",
-                        style: TextStyle(
-                          fontSize: 14,
-                          letterSpacing: 1,
-                          color: context.colors.onSurfaceVariant,
-                        ),
-                      ),
-                    ],
-                  ),
                 )
               ],
             ),
