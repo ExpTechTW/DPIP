d0de003080799ff84b300983f69af0ce24c36dc2
YuYu1015 <whes1015@gmail.com>
2024-07-16 14:51:00 +0800

    fix: ios location fetch

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,31 +1,15 @@
 import UIKit
 import Flutter
 import Firebase
-import CoreLocation
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, URLSessionDelegate {
-    var locationManager: YourLocationManagerClass?
-
+@objc class AppDelegate: FlutterAppDelegate {
     override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        FirebaseApp.configure()
-        GeneratedPluginRegistrant.register(with: self)
-
-        locationManager = YourLocationManagerClass.shared
-        locationManager?.startMonitoringSignificantLocationChanges()
-
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    override func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
-        super.application(application, handleEventsForBackgroundURLSession: identifier, completionHandler: completionHandler)
-
-        let backgroundConfigObject = URLSessionConfiguration.background(withIdentifier: identifier)
-        let backgroundSession = URLSession(configuration: backgroundConfigObject, delegate: self, delegateQueue: nil)
-        // Store the completion handler for later use
-//        YourLocationManagerClass.shared.setBackgroundCompletionHandler(completionHandler)
+      FirebaseApp.configure()
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
