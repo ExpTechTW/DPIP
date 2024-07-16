8fe4a8fb1ee02464e1f8ba9fe5f48e2f34da3f61
Yoyochou0901 <yoyochou0901@gmail.com>
2024-07-16 16:48:45 +0800

    fix: 海嘯api

--- lib/model/tsunami/tsunami_list.dart
+++ lib/model/tsunami/tsunami_list.dart
@@ -7,4 +7,6 @@ class TsunamiList {
   final String id;
 
   TsunamiList({required this.id});
+  factory TsunamiList.fromJson(Map<String, dynamic> json) => _$TsunamiListFromJson(json);
+  Map<String, dynamic> toJson() => _$TsunamiListToJson(this);
 }
