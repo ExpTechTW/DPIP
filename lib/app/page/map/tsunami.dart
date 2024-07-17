801bb613ca5e7c592e7d959c0906fbca06787dda
Yoyochou0901 <yoyochou0901@gmail.com>
2024-07-18 00:16:50 +0800

    fix: 無資料時更新時間

--- lib/app/page/map/tsunami.dart
+++ lib/app/page/map/tsunami.dart
@@ -69,6 +69,16 @@ class _TsunamiMapState extends State<TsunamiMap> {
     return "$latFormat　$lonFormat";
   }
 
+  getTime() {
+    DateTime now = DateTime.now();
+
+    DateFormat formatter = DateFormat('yyyy/MM/dd HH:mm');
+
+    String formattedDate = formatter.format(now);
+
+    return (formattedDate);
+  }
+
   @override
   void initState() {
     super.initState();
@@ -129,7 +139,7 @@ class _TsunamiMapState extends State<TsunamiMap> {
                                   )
                                 : Container(),
                             Text(
-                              "2024/07/01 00:00 $tsunamiStatus",
+                              tsunami != null ? "2024/07/01 00:00 $tsunamiStatus" : "${getTime()} 更新",
                               style: TextStyle(
                                 fontSize: 14,
                                 letterSpacing: 1,
