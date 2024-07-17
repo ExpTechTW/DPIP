46686999fc0e3d4db4490282f9195744fd3d4d17
whes1015 <whes1015@gmail.com>
2024-07-18 02:42:07 +0800

    fix: sm auto start

--- lib/route/settings/content/location.dart
+++ lib/route/settings/content/location.dart
@@ -233,31 +233,12 @@ class _SettingsLocationViewState extends State<SettingsLocationView> with Widget
             return true;
           }
         } else {
-          return await showDialog<bool>(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    icon: const Icon(Symbols.error),
-                    title: const Text("錯誤"),
-                    content: const Text("無法在此設備上啟用自動定位服務。"),
-                    actions: [
-                      TextButton(
-                        child: const Text("知道了"),
-                        onPressed: () {
-                          Navigator.pop(context, false);
-                        },
-                      ),
-                    ],
-                  );
-                },
-              ) ??
-              false;
+          return true;
         }
       } else {
         return true;
       }
     } catch (err) {
-      print(err);
       return false;
     }
   }
