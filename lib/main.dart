392fe5dadbaceba28f2a00ad578bafc546edbc0d
嚴取裂痕 <95209282+cracks666666@users.noreply.github.com>
2024-07-22 23:46:20 +0800

    fix-導覽列透明效果 (#173)

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:timezone/data/latest.dart';
+import 'package:flutter/services.dart';
 
 import 'core/service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   await Global.init();
   initializeTimeZones();
   runApp(
