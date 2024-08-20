61272e60c29ad1bb0ae35a824f953c0354dc7bb0
whes1015 <whes1015@gmail.com>
2024-08-20 17:25:45 +0800

    fix: radar

--- lib/app/page/map/radar/radar.dart
+++ lib/app/page/map/radar/radar.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:dpip/widget/list/time_selector.dart';
 import 'package:dpip/widget/map/map.dart';
 import 'package:flutter/material.dart';
+import 'package:dpip/global.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
-
 import 'package:dpip/api/exptech.dart';
+import 'package:dpip/core/ios_get_location.dart';
+import 'package:dpip/util/map_utils.dart';
 
 class RadarMap extends StatefulWidget {
   const RadarMap({Key? key}) : super(key: key);
@@ -16,13 +20,24 @@ class _RadarMapState extends State<RadarMap> {
   late MapLibreMapController _mapController;
 
   List<String> radar_list = [];
+  double userLat = 0;
+  double userLon = 0;
+  bool isUserLocationValid = false;
 
   String getTileUrl(String timestamp) {
     return "https://api-1.exptech.dev/api/v1/tiles/radar/$timestamp/{z}/{x}/{y}.png";
   }
 
+  Future<void> _loadMapImages(bool isDark) async {
+    await loadGPSImage(_mapController);
+  }
+
   void _initMap(MapLibreMapController controller) async {
     _mapController = controller;
+
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    await _loadMapImages(isDark);
 
     radar_list = await ExpTech().getRadarList();
 
@@ -36,6 +51,54 @@ class _RadarMapState extends State<RadarMap> {
         ));
 
     _mapController.addLayer("radarSource", "radarLayer", const RasterLayerProperties());
+
+    if (Platform.isIOS && (Global.preference.getBool("auto-location") ?? false)) {
+      await getSavedLocation();
+    }
+    userLat = Global.preference.getDouble("user-lat") ?? 0.0;
+    userLon = Global.preference.getDouble("user-lon") ?? 0.0;
+
+    isUserLocationValid = (userLon == 0 || userLat == 0) ? false : true;
+
+    if (isUserLocationValid) {
+      await _mapController.addSource(
+          "markers-geojson", const GeojsonSourceProperties(data: {"type": "FeatureCollection", "features": []}));
+      await _mapController.addLayer(
+        "markers-geojson",
+        "markers",
+        const SymbolLayerProperties(
+          symbolZOrder: "source",
+          iconSize: [
+            Expressions.interpolate,
+            ["linear"],
+            [Expressions.zoom],
+            5,
+            0.5,
+            10,
+            1.5,
+          ],
+          iconImage: "gps",
+          iconAllowOverlap: true,
+          iconIgnorePlacement: true,
+        ),
+      );
+      await _mapController.setGeoJsonSource(
+        "markers-geojson",
+        {
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "coordinates": [userLon, userLat],
+                "type": "Point"
+              }
+            }
+          ],
+        },
+      );
+    }
 
     setState(() {});
   }
