205bad2fff65ae14cbfdaaa6b75a409fbadc355e
whes1015 <whes1015@gmail.com>
2024-08-25 02:44:08 +0800

    fix: network check

--- lib/app/dpip.dart
+++ lib/app/dpip.dart
@@ -1,26 +1,26 @@
-import "dart:io";
+import 'dart:io';
 
-import "package:dpip/api/exptech.dart";
-import "package:dpip/app/page/history/history.dart";
-import "package:dpip/app/page/home/home.dart";
-import "package:dpip/app/page/map/map.dart";
-import "package:dpip/app/page/me/me.dart";
-import "package:dpip/app/page/more/more.dart";
-import "package:dpip/core/fcm.dart";
-import "package:dpip/core/notify.dart";
-import "package:dpip/core/service.dart";
-import "package:dpip/global.dart";
-import "package:dpip/route/announcement/announcement.dart";
-import "package:dpip/route/changelog/changelog.dart";
-import "package:dpip/route/update_required/update_required.dart";
-import "package:dpip/route/welcome/about.dart";
-import "package:dpip/util/extension/build_context.dart";
-import "package:flutter/material.dart";
-import "package:in_app_update/in_app_update.dart";
-import "package:material_symbols_icons/symbols.dart";
+import 'package:dpip/api/exptech.dart';
+import 'package:dpip/app/page/history/history.dart';
+import 'package:dpip/app/page/home/home.dart';
+import 'package:dpip/app/page/map/map.dart';
+import 'package:dpip/app/page/me/me.dart';
+import 'package:dpip/app/page/more/more.dart';
+import 'package:dpip/core/fcm.dart';
+import 'package:dpip/core/notify.dart';
+import 'package:dpip/core/service.dart';
+import 'package:dpip/global.dart';
+import 'package:dpip/route/changelog/changelog.dart';
+import 'package:dpip/route/update_required/update_required.dart';
+import 'package:dpip/route/welcome/about.dart';
+import 'package:dpip/util/extension/build_context.dart';
+import 'package:flutter/material.dart';
+import 'package:in_app_update/in_app_update.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:dpip/route/announcement/announcement.dart';
 
 class Dpip extends StatefulWidget {
-  const Dpip({Key? key}) : super(key: key);
+  const Dpip({super.key});
 
   @override
   State<Dpip> createState() => _DpipState();
@@ -121,142 +121,197 @@ class _DpipState extends State<Dpip> {
           }
         } else {
           if (Global.preference.getString("changelog") != Global.packageInfo.version) {
-            Global.preference.setString("changelog", Global.packageInfo.version);
-            await showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (context) {
-                return AlertDialog(
-                  icon: const Icon(Symbols.update_rounded),
-                  title: Text(context.i18n.update_complete),
-                  content: Text(
-                    context.i18n.update_complete_prompt,
-                  ),
-                  actionsAlignment: MainAxisAlignment.spaceBetween,
-                  actions: [
-                    TextButton(
-                      child: Text(context.i18n.remind_later),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    TextButton(
-                      child: Text(context.i18n.go_to_view),
-                      onPressed: () {
-                        Navigator.pop(context);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ChangelogPage()),
-                        );
-                      },
-                    ),
-                  ],
-                );
-              },
-            );
+            await _showChangelogDialog();
           } else {
-            var data = await ExpTech().getAnnouncement();
-            if (data.last.show && Global.preference.getString("announcement") != data.last.time.toString()) {
-              Global.preference.setString("announcement", data.last.time.toString());
-              await showDialog(
-                context: context,
-                barrierDismissible: false,
-                builder: (context) {
-                  return AlertDialog(
-                    icon: const Icon(Symbols.announcement),
-                    title: Text(context.i18n.announcement),
-                    content: Text(
-                      context.i18n.new_announcement_prompt,
-                    ),
-                    actionsAlignment: MainAxisAlignment.spaceBetween,
-                    actions: [
-                      TextButton(
-                        child: Text(context.i18n.remind_later),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      TextButton(
-                        child: Text(context.i18n.go_to_view),
-                        onPressed: () {
-                          Navigator.pop(context);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const AnnouncementPage()),
-                          );
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-            }
+            await _checkAnnouncement();
           }
-          fcmInit();
-          notifyInit();
-          initBackgroundService();
         }
       }
-    } catch (e) {
-      print("Error checking for updates: $e");
-      update = false;
-      criticalUpdate = false;
-    } finally {
       if (mounted) {
         setState(() {});
       }
+    } catch (e) {
+      print("Error checking for updates: $e");
+      await _showErrorDialog();
     }
+  }
+
+  Future<void> _showChangelogDialog() async {
+    Global.preference.setString("changelog", Global.packageInfo.version);
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return WillPopScope(
+          onWillPop: () async => false,
+          child: AlertDialog(
+            icon: const Icon(Symbols.update_rounded),
+            title: Text(context.i18n.update_complete),
+            content: Text(context.i18n.update_complete_prompt),
+            actionsAlignment: MainAxisAlignment.spaceBetween,
+            actions: [
+              TextButton(
+                child: Text(context.i18n.remind_later),
+                onPressed: () {
+                  Navigator.pop(context);
+                  _checkAnnouncement();
+                },
+              ),
+              TextButton(
+                child: Text(context.i18n.go_to_view),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ChangelogPage()),
+                  ).then((_) => _checkAnnouncement());
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _checkAnnouncement() async {
+    var data = await ExpTech().getAnnouncement();
+    if (data.last.show && Global.preference.getString("announcement") != data.last.time.toString()) {
+      Global.preference.setString("announcement", data.last.time.toString());
+      if (mounted) {
+        await showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) {
+            return WillPopScope(
+              onWillPop: () async => false,
+              child: AlertDialog(
+                icon: const Icon(Symbols.announcement),
+                title: Text(context.i18n.announcement),
+                content: Text(context.i18n.new_announcement_prompt),
+                actionsAlignment: MainAxisAlignment.spaceBetween,
+                actions: [
+                  TextButton(
+                    child: Text(context.i18n.remind_later),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _initializeServices();
+                    },
+                  ),
+                  TextButton(
+                    child: Text(context.i18n.go_to_view),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const AnnouncementPage()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      }
+    } else {
+      _initializeServices();
+    }
+  }
+
+  void _initializeServices() {
+    fcmInit();
+    notifyInit();
+    initBackgroundService();
+  }
+
+  Future<void> _showErrorDialog() async {
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return WillPopScope(
+          onWillPop: () async => false,
+          child: AlertDialog(
+            icon: const Icon(Symbols.signal_disconnected_rounded),
+            title: Text("異常"),
+            content: Text("網路連線或伺服器異常。"),
+            actionsAlignment: MainAxisAlignment.spaceBetween,
+            actions: [
+              TextButton(
+                child: Text("重試"),
+                onPressed: () {
+                  Navigator.pop(context);
+                  _restartApp();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _restartApp() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => const Dpip()),
+      (Route<dynamic> route) => false,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: currentActivePage,
-        destinations: [
-          NavigationDestination(
-            icon: const Icon(Symbols.home),
-            selectedIcon: const Icon(Symbols.home, fill: 1),
-            label: context.i18n.home,
-          ),
-          NavigationDestination(
-            icon: const Icon(Symbols.clock_loader_10_rounded),
-            selectedIcon: const Icon(Symbols.clock_loader_10_rounded, fill: 1),
-            label: context.i18n.history,
-          ),
-          NavigationDestination(
-            icon: const Icon(Symbols.map),
-            selectedIcon: const Icon(Symbols.map, fill: 1),
-            label: context.i18n.map,
-          ),
-          NavigationDestination(
-            icon: const Icon(Symbols.note_stack_add_rounded),
-            selectedIcon: const Icon(Symbols.note_stack_add_rounded, fill: 1),
-            label: context.i18n.more,
-          ),
-          NavigationDestination(
-            icon: const Icon(Symbols.person),
-            selectedIcon: const Icon(Symbols.person, fill: 1),
-            label: context.i18n.me,
-          ),
-        ],
-        onDestinationSelected: (value) {
-          setState(() {
-            currentActivePage = value;
-          });
-          controller.jumpToPage(currentActivePage);
-        },
-      ),
-      body: PageView(
-        controller: controller,
-        physics: const NeverScrollableScrollPhysics(),
-        children: const [
-          HomePage(),
-          HistoryPage(),
-          MapPage(),
-          MorePage(),
-          MePage(),
-        ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: currentActivePage,
+          destinations: [
+            NavigationDestination(
+              icon: const Icon(Symbols.home),
+              selectedIcon: const Icon(Symbols.home, fill: 1),
+              label: context.i18n.home,
+            ),
+            NavigationDestination(
+              icon: const Icon(Symbols.clock_loader_10_rounded),
+              selectedIcon: const Icon(Symbols.clock_loader_10_rounded, fill: 1),
+              label: context.i18n.history,
+            ),
+            NavigationDestination(
+              icon: const Icon(Symbols.map),
+              selectedIcon: const Icon(Symbols.map, fill: 1),
+              label: context.i18n.map,
+            ),
+            NavigationDestination(
+              icon: const Icon(Symbols.note_stack_add_rounded),
+              selectedIcon: const Icon(Symbols.note_stack_add_rounded, fill: 1),
+              label: context.i18n.more,
+            ),
+            NavigationDestination(
+              icon: const Icon(Symbols.person),
+              selectedIcon: const Icon(Symbols.person, fill: 1),
+              label: context.i18n.me,
+            ),
+          ],
+          onDestinationSelected: (value) {
+            setState(() {
+              currentActivePage = value;
+            });
+            controller.jumpToPage(currentActivePage);
+          },
+        ),
+        body: PageView(
+          controller: controller,
+          physics: const NeverScrollableScrollPhysics(),
+          children: const [
+            HomePage(),
+            HistoryPage(),
+            MapPage(),
+            MorePage(),
+            MePage(),
+          ],
+        ),
       ),
     );
   }
