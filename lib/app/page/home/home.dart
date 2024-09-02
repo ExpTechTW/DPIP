df843920075c6cc8409f26b851e686a237cebade
Yoyochou0901 <yoyochou0901@gmail.com>
2024-09-02 21:11:38 +0800

    fix: weather icon

--- lib/app/page/home/home.dart
+++ lib/app/page/home/home.dart
@@ -216,7 +216,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   Widget _buildTemperatureDisplay() {
     final tempParts = (weatherData['weather']?['data']?['air']?['temperature'] ?? '--').toString().split('.');
-    final weatherCode = weatherData['weather']?['weatherCode'] ?? 100;
+    final weatherCode = weatherData['weather']['data']?['weatherCode'] ?? 100;
     const isDay = 1;
 
     return Padding(
