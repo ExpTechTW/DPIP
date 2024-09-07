fa9f67a7635317df087476b189b34cfcc28aae66
whes1015 <whes1015@gmail.com>
2024-09-07 11:49:07 +0800

    feat: temp rank

--- lib/app/page/more/ranking/tabs/precipitation.dart
+++ lib/app/page/more/ranking/tabs/precipitation.dart
@@ -4,6 +4,8 @@ import 'package:collection/collection.dart';
 import 'package:dpip/app/page/more/ranking/widgets/ranking_list.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:dpip/api/exptech.dart';
+import 'package:dpip/model/weather/rain.dart';
 
 class RankingPrecipitationTab extends StatefulWidget {
   const RankingPrecipitationTab({super.key});
@@ -14,8 +16,66 @@ class RankingPrecipitationTab extends StatefulWidget {
 
 class _RankingPrecipitationTabState extends State<RankingPrecipitationTab> {
   List<int> data = [];
+  List<String> rainTimeList = [];
+  List<RainData> rainDataList = [];
 
   Future refresh() async {
+    rainTimeList = await ExpTech().getRainList();
+
+    List<RainStation> rainData = await ExpTech().getRain(rainTimeList.last);
+
+    // rainDataList = rainData
+    //     .map((station) {
+    //   double rainfall;
+    //   switch (interval) {
+    //     case "now":
+    //       rainfall = station.data.now;
+    //       break;
+    //     case "10m":
+    //       rainfall = station.data.tenMinutes;
+    //       break;
+    //     case "1h":
+    //       rainfall = station.data.oneHour;
+    //       break;
+    //     case "3h":
+    //       rainfall = station.data.threeHours;
+    //       break;
+    //     case "6h":
+    //       rainfall = station.data.sixHours;
+    //       break;
+    //     case "12h":
+    //       rainfall = station.data.twelveHours;
+    //       break;
+    //     case "24h":
+    //       rainfall = station.data.twentyFourHours;
+    //       break;
+    //     case "2d":
+    //       rainfall = station.data.twoDays;
+    //       break;
+    //     case "3d":
+    //       rainfall = station.data.threeDays;
+    //       break;
+    //     default:
+    //       rainfall = station.data.now;
+    //   }
+    //
+    //   if (rainfall == -99) {
+    //     return null;
+    //   }
+    //
+    //   return RainData(
+    //     id: station.id,
+    //     latitude: station.station.lat,
+    //     longitude: station.station.lng,
+    //     rainfall: rainfall,
+    //     stationName: station.station.name,
+    //     county: station.station.county,
+    //     town: station.station.town,
+    //   );
+    // })
+    //     .whereType<RainData>()
+    //     .toList();
+
     setState(() {
       data = List.generate(200, (index) => Random().nextInt((index + 1) * (index + 1))).sorted((a, b) => b - a);
     });
--- lib/app/page/more/ranking/tabs/temperature.dart
+++ lib/app/page/more/ranking/tabs/temperature.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:dpip/api/exptech.dart';
+import 'package:dpip/model/weather/weather.dart';
+import 'package:dpip/app/page/more/ranking/widgets/ranking_list.dart';
 
 class RankingTemperatureTab extends StatefulWidget {
   const RankingTemperatureTab({super.key});
@@ -8,8 +12,55 @@ class RankingTemperatureTab extends StatefulWidget {
 }
 
 class _RankingTemperatureTabState extends State<RankingTemperatureTab> {
+  List<WeatherStation> validWeatherStations = [];
+
+  Future<void> refresh() async {
+    final weatherList = await ExpTech().getWeatherList();
+    final latestWeatherData = await ExpTech().getWeather(weatherList.last);
+
+    setState(() {
+      validWeatherStations = latestWeatherData
+          .where((station) => station.data.air.temperature != -99)
+          .toList();
+      validWeatherStations.sort((a, b) => b.data.air.temperature.compareTo(a.data.air.temperature));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    refresh();
+  }
+
+  Widget _buildListItem(WeatherStation station, int rank) {
+    return Row(
+      children: [
+        _buildRankIndicator(rank),
+        const SizedBox(width: 8),
+        Expanded(
+          child: Text(
+            "${station.station.name}: ${station.data.air.temperature.toStringAsFixed(1)}°C",
+            style: TextStyle(fontSize: rank == 1 ? 20 : 16),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRankIndicator(int rank) {
+    return rank == 1
+        ? const Icon(Symbols.trophy_rounded)
+        : Text("$rank");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RefreshIndicator(
+      onRefresh: refresh,
+      child: RankingList(
+        data: validWeatherStations,
+        contentBuilder: (context, station, rank) => _buildListItem(station, rank),
+      ),
+    );
   }
 }
