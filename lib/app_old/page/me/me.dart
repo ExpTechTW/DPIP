8700f5e303a995033903513ba1510ff5e896100e
Kamiya <kamiya@kamiya.app>
2025-05-01 14:23:03 +0800

    chore: remove old list items

--- lib/app_old/page/me/me.dart
+++ lib/app_old/page/me/me.dart
@@ -1,16 +1,8 @@
-import "dart:io";
-
 import "package:flutter/material.dart";
 
-import "package:clipboard/clipboard.dart";
 import "package:go_router/go_router.dart";
 import "package:material_symbols_icons/symbols.dart";
-import "package:url_launcher/url_launcher.dart";
 
-import "package:dpip/global.dart";
-import "package:dpip/route/announcement/announcement.dart";
-import "package:dpip/route/changelog/changelog.dart";
-import "package:dpip/route/log/log.dart";
 import "package:dpip/route/sound/sound.dart";
 import "package:dpip/utils/extensions/build_context.dart";
 import "package:dpip/widgets/list/tile_group_header.dart";
@@ -55,60 +47,6 @@ class _MePageState extends State<MePage> {
                   builder: (context) => const SoundRoute(),
                 ),
               ),
-        ),
-
-        /**
-         * 更新日誌
-         */
-        ListTile(
-          leading: const Icon(Symbols.campaign_rounded, fill: 1),
-          title: Text(context.i18n.announcement),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const AnnouncementPage()));
-          },
-        ),
-        ListTile(
-          leading: const Icon(Symbols.update_rounded),
-          title: Text(context.i18n.update_log),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const ChangelogPage()));
-          },
-        ),
-        ListTile(
-          leading: const Icon(Symbols.favorite_rounded, fill: 1),
-          title: Text(context.i18n.donate),
-          subtitle: Text(context.i18n.donate_h2),
-          onTap: () {
-            launchUrl(Uri.parse("https://exptech.com.tw/donate"));
-          },
-        ),
-        ListTileGroupHeader(title: context.i18n.me_debug),
-        ListTile(
-          leading: const Icon(Symbols.bug_report),
-          title: Text(context.i18n.app_logs),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => LogViewerPage()));
-          },
-        ),
-        /**
-         * 複製 FCM Token
-         */
-        ListTile(
-          leading: const Icon(Icons.fingerprint),
-          title: Text(context.i18n.settings_fcm),
-          onTap: () {
-            String token =
-                ((Platform.isIOS)
-                    ? Global.preference.getString("apns-token")
-                    : Global.preference.getString("fcm-token")) ??
-                "";
-            if (token != "") {
-              FlutterClipboard.copy(token);
-              context.scaffoldMessenger.showSnackBar(SnackBar(content: Text(context.i18n.settings_copy_fcm)));
-            } else {
-              context.scaffoldMessenger.showSnackBar(SnackBar(content: Text(context.i18n.copy_fcm_token_error)));
-            }
-          },
         ),
         ListTileGroupHeader(title: context.i18n.me_about),
         ListTile(
