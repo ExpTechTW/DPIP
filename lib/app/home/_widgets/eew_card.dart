fa533d5346f5d7258d1ea96b6f1d5ef6b062e87c
Kamiya <kamiya@kamiya.app>
2025-05-02 12:35:11 +0800

    feat: selector

--- lib/app/home/_widgets/eew_card.dart
+++ lib/app/home/_widgets/eew_card.dart
@@ -1,9 +1,11 @@
 import 'package:dpip/app/map/monitor/monitor.dart';
+import 'package:dpip/models/settings/location.dart';
 import 'package:dpip/utils/extensions/build_context.dart';
 import 'package:dpip/utils/extensions/int.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
+import 'package:provider/provider.dart';
 
 class EewCard extends StatelessWidget {
   const EewCard({super.key});
@@ -26,135 +28,158 @@ class EewCard extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 8,
             children: [
               Row(
-                mainAxisSize: MainAxisSize.min,
-                spacing: 8,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(color: context.colors.error, borderRadius: BorderRadius.circular(8)),
-                    padding: const EdgeInsets.fromLTRB(8, 6, 12, 6),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      spacing: 4,
-                      children: [
-                        Icon(Symbols.crisis_alert_rounded, color: context.colors.onError, weight: 700, size: 22),
-                        Text(
-                          context.i18n.emergency_earthquake_warning,
-                          style: context.textTheme.labelLarge!.copyWith(
-                            color: context.colors.onError,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  Row(
+                    spacing: 8,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(color: context.colors.error, borderRadius: BorderRadius.circular(8)),
+                        padding: const EdgeInsets.fromLTRB(8, 6, 12, 6),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          spacing: 4,
+                          children: [
+                            Icon(Symbols.crisis_alert_rounded, color: context.colors.onError, weight: 700, size: 22),
+                            Text(
+                              context.i18n.emergency_earthquake_warning,
+                              style: context.textTheme.labelLarge!.copyWith(
+                                color: context.colors.onError,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                      Text(
+                        '第 2 報',
+                        style: context.textTheme.bodyLarge!.copyWith(color: context.colors.onErrorContainer),
+                      ),
+                    ],
                   ),
+                  Icon(Symbols.chevron_right_rounded, color: context.colors.onErrorContainer, size: 24),
                 ],
               ),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(text: '04/25 06:10 左右 '),
-                    TextSpan(text: '嘉義縣大埔鄉', style: TextStyle(fontWeight: FontWeight.bold)),
-                    TextSpan(text: ' 附近發生有感地震，預估規模 '),
-                    TextSpan(text: '4.5', style: TextStyle(fontWeight: FontWeight.bold)),
-                    TextSpan(text: '、最大震度 '),
-                    TextSpan(text: '4級', style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                  style: context.textTheme.bodyLarge!.copyWith(color: context.colors.onErrorContainer),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(text: '04/25 06:10 左右 '),
+                      TextSpan(text: '嘉義縣大埔鄉', style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ' 附近發生有感地震，預估規模 '),
+                      TextSpan(text: '4.5', style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: '、最大震度 '),
+                      TextSpan(text: '4級', style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                    style: context.textTheme.bodyLarge!.copyWith(color: context.colors.onErrorContainer),
+                  ),
                 ),
               ),
-              IntrinsicHeight(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(4),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          spacing: 16,
-                          children: [
-                            Text(
-                              context.i18n.location_estimate,
-                              style: context.textTheme.labelLarge!.copyWith(
-                                color: context.colors.onErrorContainer.withValues(alpha: 0.6),
-                              ),
-                            ),
+              Selector<SettingsLocationModel, String?>(
+                selector: (context, model) => model.code,
+                builder: (context, code, child) {
+                  if (code == null) {
+                    return const SizedBox.shrink();
+                  }
 
-                            RichText(
-                              text: TextSpan(
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                spacing: 16,
                                 children: [
-                                  TextSpan(
-                                    text: 2.asIntensityDisplayLabel,
-                                    style: context.textTheme.displayLarge!.copyWith(
-                                      fontWeight: FontWeight.bold,
-                                      color: context.colors.onErrorContainer,
+                                  Text(
+                                    context.i18n.location_estimate,
+                                    style: context.textTheme.labelLarge!.copyWith(
+                                      color: context.colors.onErrorContainer.withValues(alpha: 0.6),
                                     ),
                                   ),
-                                  TextSpan(
-                                    text: ' 級',
-                                    style: context.textTheme.bodyLarge!.copyWith(
-                                      color: context.colors.onErrorContainer,
+
+                                  RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 2.asIntensityDisplayLabel,
+                                          style: context.textTheme.displayLarge!.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                            color: context.colors.onErrorContainer,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: ' 級',
+                                          style: context.textTheme.bodyLarge!.copyWith(
+                                            color: context.colors.onErrorContainer,
+                                          ),
+                                        ),
+                                      ],
                                     ),
+                                    textAlign: TextAlign.center,
                                   ),
                                 ],
                               ),
-                              textAlign: TextAlign.center,
                             ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    VerticalDivider(
-                      color: context.colors.onErrorContainer.withValues(alpha: 0.2),
-                      indent: 8,
-                      endIndent: 8,
-                      width: 24,
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(4),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          spacing: 16,
-                          children: [
-                            Text(
-                              context.i18n.seismic_waves,
-                              style: context.textTheme.labelLarge!.copyWith(
-                                color: context.colors.onErrorContainer.withValues(alpha: 0.6),
-                              ),
-                            ),
-                            RichText(
-                              text: TextSpan(
+                          ),
+                          VerticalDivider(
+                            color: context.colors.onErrorContainer.withValues(alpha: 0.4),
+                            indent: 8,
+                            endIndent: 8,
+                            width: 24,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                spacing: 16,
                                 children: [
-                                  TextSpan(
-                                    text: '8',
-                                    style: context.textTheme.displayLarge!.copyWith(
-                                      fontWeight: FontWeight.bold,
-                                      color: context.colors.onErrorContainer,
+                                  Text(
+                                    context.i18n.seismic_waves,
+                                    style: context.textTheme.labelLarge!.copyWith(
+                                      color: context.colors.onErrorContainer.withValues(alpha: 0.6),
                                     ),
                                   ),
-                                  TextSpan(
-                                    text: ' ${context.i18n.monitor_after_seconds}',
-                                    style: context.textTheme.bodyLarge!.copyWith(
-                                      color: context.colors.onErrorContainer,
+                                  RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: '8',
+                                          style: context.textTheme.displayLarge!.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                            color: context.colors.onErrorContainer,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: ' ${context.i18n.monitor_after_seconds}',
+                                          style: context.textTheme.bodyLarge!.copyWith(
+                                            color: context.colors.onErrorContainer,
+                                          ),
+                                        ),
+                                      ],
                                     ),
+                                    textAlign: TextAlign.center,
                                   ),
                                 ],
                               ),
-                              textAlign: TextAlign.center,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
             ],
           ),
