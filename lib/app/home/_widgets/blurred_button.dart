7083206ecf54994ef027764cc803b142b5f43ad6
Kamiya <kamiya@kamiya.app>
2025-05-04 14:37:01 +0800

    feat: navgation buttons

---
+++ lib/app/home/_widgets/blurred_button.dart
@@ -0,0 +1,83 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'package:dpip/utils/extensions/build_context.dart';
+
+class BlurredTextButton extends StatelessWidget {
+  final String text;
+  final void Function()? onPressed;
+  final Color? backgroundColor;
+  final double sigmaX;
+  final double sigmaY;
+  final TextStyle? textStyle;
+
+  const BlurredTextButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.backgroundColor,
+    this.textStyle,
+    this.sigmaX = 8,
+    this.sigmaY = 8,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(double.maxFinite)),
+      clipBehavior: Clip.hardEdge,
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: backgroundColor ?? context.colors.surfaceContainerHigh.withValues(alpha: 0.6),
+            foregroundColor: context.colors.outline,
+            textStyle: textStyle,
+          ),
+          onPressed: onPressed,
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
+
+class BlurredIconButton extends StatelessWidget {
+  final Widget icon;
+  final void Function()? onPressed;
+  final Color? backgroundColor;
+  final double sigmaX;
+  final double sigmaY;
+
+  const BlurredIconButton({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+    this.backgroundColor,
+    this.sigmaX = 8,
+    this.sigmaY = 8,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(shape: BoxShape.circle),
+      clipBehavior: Clip.hardEdge,
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
+        child: IconButton(
+          style: IconButton.styleFrom(
+            backgroundColor: backgroundColor ?? context.colors.surfaceContainerHigh.withValues(alpha: 0.6),
+            foregroundColor: context.colors.outline,
+          ),
+          onPressed: onPressed,
+          icon: icon,
+        ),
+      ),
+    );
+  }
+}
--- lib/app/layout.dart
+++ lib/app/layout.dart
@@ -1,16 +1,12 @@
-import 'dart:ui';
-
+import 'package:dpip/app/home/_widgets/blurred_button.dart';
+import 'package:dpip/utils/extensions/build_context.dart';
 import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
 
-import 'package:dpip/utils/extensions/build_context.dart';
-
-class NavigationLocation extends NavigationDestination {
-  final String location;
-
-  NavigationLocation({super.key, required this.location, required Icon super.icon, required super.label})
+class NavigationLocation extends NavigationDrawerDestination {
+  NavigationLocation({super.key, required Icon super.icon, required super.label})
     : super(selectedIcon: Icon(icon.icon, fill: 1));
 }
 
@@ -18,33 +14,35 @@ class AppLayout extends StatelessWidget {
   final String location;
   final StatefulNavigationShell navigationShell;
 
-  const AppLayout({super.key, required this.location, required this.navigationShell});
+  AppLayout({super.key, required this.location, required this.navigationShell});
 
-  List<NavigationLocation> get locations => [
-    NavigationLocation(location: '/home', icon: Icon(Symbols.home_rounded), label: 'Home'),
-    NavigationLocation(location: '/history', icon: Icon(Symbols.clock_loader_10_rounded), label: 'History'),
-    NavigationLocation(location: '/map', icon: Icon(Symbols.map_rounded), label: 'Map'),
-    NavigationLocation(location: '/more', icon: Icon(Symbols.note_stack_add_rounded), label: 'More'),
-    NavigationLocation(location: '/me', icon: Icon(Symbols.person_rounded), label: 'Me'),
-  ];
+  final _scaffold = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: navigationShell,
-      bottomNavigationBar: SizedBox(
-        height: 80 + context.padding.bottom,
-        child: ClipRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-            child: NavigationBar(
-              backgroundColor: context.colors.surfaceContainerHigh.withValues(alpha: 0.8),
-              selectedIndex: navigationShell.currentIndex,
-              destinations: locations,
-              onDestinationSelected: (value) => navigationShell.goBranch(value),
+      key: _scaffold,
+      body: Stack(
+        children: [
+          navigationShell,
+          Positioned(
+            top: 24,
+            left: 24,
+            child: SafeArea(
+              child: BlurredIconButton(onPressed: () => context.push('/map/monitor'), icon: Icon(Symbols.map_rounded)),
             ),
           ),
-        ),
+          Positioned(
+            top: 24,
+            right: 24,
+            child: SafeArea(
+              child: BlurredIconButton(
+                onPressed: () => context.push('/settings'),
+                icon: Icon(Symbols.settings_rounded),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
