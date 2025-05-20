962a23a980ae509fb58b37ea032364d70128d2a0
lowrt <nineder197789@gmail.com>
2025-05-20 13:27:35 +0800

    fix(Android):prevent double splash screen on Android 12+

--- android/app/src/main/kotlin/com/exptech/dpip/MainActivity.kt
+++ android/app/src/main/kotlin/com/exptech/dpip/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.exptech.dpip
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
