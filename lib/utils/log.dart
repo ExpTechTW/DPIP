0220afbbdb7281d89acb7944744514a0151f58e3
Kamiya <kamiya@kamiya.app>
2025-05-26 10:38:50 +0800

    chore: cleaner logs

--- lib/utils/log.dart
+++ lib/utils/log.dart
@@ -1,9 +1,22 @@
 import 'package:talker_flutter/talker_flutter.dart';
 
+class CustomLoggerFormatter implements LoggerFormatter {
+  const CustomLoggerFormatter();
+
+  @override
+  String fmt(LogDetails details, TalkerLoggerSettings settings) {
+    final msg = details.message?.toString() ?? '';
+    if (!settings.enableColors) {
+      return msg;
+    }
+    return msg.split('\n').map((e) => details.pen.write(e)).join('\n');
+  }
+}
+
 class TalkerManager {
   TalkerManager._();
 
-  static final Talker _instance = Talker();
+  static final Talker _instance = Talker(logger: TalkerLogger(formatter: const CustomLoggerFormatter()));
 
   static Talker get instance => _instance;
 }
