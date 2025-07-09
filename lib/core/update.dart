77596b0bf842d9a31f69f1482300d3079da185cd
YuYu1015 <whes1015@gmail.com>
2025-07-09 23:00:03 +0800

    fix: updateInfoToServer

--- lib/core/update.dart
+++ lib/core/update.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
 import 'package:dpip/api/exptech.dart';
 import 'package:dpip/core/preference.dart';
 
 Future<void> updateInfoToServer() async {
-  int now = DateTime.now().millisecondsSinceEpoch;
-  if (Preference.notifyToken != '' && now - (Preference.lastUpdateToServerTime ?? 0) > 86400 * 3 * 1000) {
+  final int now = DateTime.now().millisecondsSinceEpoch;
+
+  if (Preference.notifyToken != '' && now - (Preference.lastUpdateToServerTime ?? 0) > 86400 * 1 * 1000) {
+    final random = Random();
+
+    final int rand = random.nextInt(3);
+
+    if (rand != 0) return;
+
     ExpTech().updateDeviceLocation(
       token: Preference.notifyToken,
       lat: Preference.locationLatitude.toString(),
