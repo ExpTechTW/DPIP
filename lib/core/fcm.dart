443a5dcfd4bfc9da38931d1d3ef6917ceaf027ae
YuYu1015 <whes1015@gmail.com>
2025-07-24 05:26:23 +0800

    fix: notify

--- lib/core/fcm.dart
+++ lib/core/fcm.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:awesome_notifications_fcm/awesome_notifications_fcm.dart';
 import 'package:dpip/core/preference.dart';
-import 'package:dpip/utils/log.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
@@ -12,8 +11,8 @@ Future<void> fcmInit() async {
 
   if (Platform.isAndroid) {
     await AwesomeNotificationsFcm().initialize(
-      onFcmTokenHandle: onFcmTokenHandle,
-      onNativeTokenHandle: onNativeTokenHandle,
+      onFcmTokenHandle: onTokenHandle,
+      onNativeTokenHandle: onTokenHandle,
       onFcmSilentDataHandle: onFcmSilentDataHandle,
       licenseKeys: [
         '2024-08-26==N0LtVWu49ox9yV8eaDrh8rGyji/iKzLaB6anluLFIPESM/rUtf0OTUDyExMB+hp8YqnfA9UMcwvT5i5lTcsB73WKbh2+cYbYwtSZoxjuSUUbNxzhnlH2uiD7CNYvtniORC69TStgEfXnYZ1dEfWe5p6Nwi4wDS7vTfyTOH2NqCW+5293ypcu6+7se2PxLGOF1s8YKbM3HU8nYk8juChbFNoxX/Y0pHOH+MvXi070o1+3SPL98BS9bPQQ0e9a9MgYpxRqthP/mT1Yx2AX4+d+Qb6NNiz8ub+rl1HhZc7vmy5bntJSwcculDhXG3YOP3uXeYYyc2L+NKqkHPYpflblOg==',
@@ -27,19 +26,14 @@ Future<void> fcmInit() async {
   }
 }
 
-Future<void> onFcmTokenHandle(String token) async {
-  Preference.notifyToken = token;
-}
-
-Future<void> onNativeTokenHandle(String token) async {
-  TalkerManager.instance.info('FCM Token:"$token"');
+Future<void> onTokenHandle(String token) async {
   Preference.notifyToken = token;
 }
 
 Future<void> onFcmSilentDataHandle(FcmSilentData silentData) async {
   final Map<String, dynamic> data = silentData.data!.cast<String, dynamic>();
 
-  if (silentData.createdLifeCycle == NotificationLifeCycle.AppKilled) {
+  if (silentData.createdLifeCycle == NotificationLifeCycle.Terminated) {
     final channelKey = (data['channel'] as String?) ?? 'other';
     data['content'] = {
       'id': int.parse((data['id'] as String?) ?? '0'),
@@ -52,7 +46,6 @@ Future<void> onFcmSilentDataHandle(FcmSilentData silentData) async {
   } else {
     await showNotify(data);
   }
-  return Future.value();
 }
 
 Future<void> showNotify(Map<String, dynamic> data) async {
