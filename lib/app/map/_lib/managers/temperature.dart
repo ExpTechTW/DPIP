9e6aebe8a973ca5c0e0707ecfce919ce8e6523d5
Kamiya <kamiya@kamiya.app>
2025-08-07 01:36:37 +0800

    feat: map legends (#347)
    * feat: map legends
    
    * feat: radar unit

--- lib/app/map/_lib/managers/temperature.dart
+++ lib/app/map/_lib/managers/temperature.dart
@@ -1,23 +1,29 @@
+import 'package:flutter/material.dart';
+
 import 'package:collection/collection.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+import 'package:provider/provider.dart';
+
 import 'package:dpip/api/exptech.dart';
 import 'package:dpip/api/model/weather/weather.dart';
 import 'package:dpip/app/map/_lib/manager.dart';
 import 'package:dpip/app/map/_lib/utils.dart';
+import 'package:dpip/app/map/_widgets/map_legend.dart';
 import 'package:dpip/core/i18n.dart';
 import 'package:dpip/core/providers.dart';
 import 'package:dpip/models/data.dart';
+import 'package:dpip/models/settings/ui.dart';
 import 'package:dpip/utils/extensions/build_context.dart';
+import 'package:dpip/utils/extensions/int.dart';
 import 'package:dpip/utils/extensions/latlng.dart';
 import 'package:dpip/utils/extensions/string.dart';
 import 'package:dpip/utils/geojson.dart';
 import 'package:dpip/utils/log.dart';
+import 'package:dpip/widgets/blurred_container.dart';
 import 'package:dpip/widgets/map/map.dart';
 import 'package:dpip/widgets/sheet/morphing_sheet.dart';
 import 'package:dpip/widgets/ui/loading_icon.dart';
-import 'package:flutter/material.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
-import 'package:provider/provider.dart';
 
 class TemperatureData {
   final double latitude;
@@ -244,98 +250,131 @@ class TemperatureMapLayerSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MorphingSheet(
-      title: '氣溫'.i18n,
-      borderRadius: BorderRadius.circular(16),
-      elevation: 4,
-      partialBuilder: (context, controller, sheetController) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Selector<DpipDataModel, UnmodifiableListView<String>>(
-            selector: (context, model) => model.temperature,
-            builder: (context, temperature, header) {
-              final times = temperature.map((time) {
-                final t = time.toSimpleDateTimeString(context).split(' ');
-                return (date: t[0], time: t[1], value: time);
-              });
-              final grouped = times.groupListsBy((time) => time.date).entries.toList();
+    return Stack(
+      children: [
+        MorphingSheet(
+          title: '氣溫'.i18n,
+          borderRadius: BorderRadius.circular(16),
+          elevation: 4,
+          partialBuilder: (context, controller, sheetController) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Selector<DpipDataModel, UnmodifiableListView<String>>(
+                selector: (context, model) => model.temperature,
+                builder: (context, temperature, header) {
+                  final times = temperature.map((time) {
+                    final t = time.toSimpleDateTimeString(context).split(' ');
+                    return (date: t[0], time: t[1], value: time);
+                  });
+                  final grouped = times.groupListsBy((time) => time.date).entries.toList();
 
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  header!,
-                  SizedBox(
-                    height: kMinInteractiveDimension,
-                    child: ValueListenableBuilder<String?>(
-                      valueListenable: manager.currentTemperatureTime,
-                      builder: (context, currentTemperatureTime, child) {
-                        return ListView.builder(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          scrollDirection: Axis.horizontal,
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          itemCount: grouped.length,
-                          itemBuilder: (context, index) {
-                            final MapEntry(key: date, value: group) = grouped[index];
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      header!,
+                      SizedBox(
+                        height: kMinInteractiveDimension,
+                        child: ValueListenableBuilder<String?>(
+                          valueListenable: manager.currentTemperatureTime,
+                          builder: (context, currentTemperatureTime, child) {
+                            return ListView.builder(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              scrollDirection: Axis.horizontal,
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              itemCount: grouped.length,
+                              itemBuilder: (context, index) {
+                                final MapEntry(key: date, value: group) = grouped[index];
 
-                            final children = <Widget>[Text(date)];
+                                final children = <Widget>[Text(date)];
 
-                            for (final time in group) {
-                              final isSelected = time.value == currentTemperatureTime;
+                                for (final time in group) {
+                                  final isSelected = time.value == currentTemperatureTime;
 
-                              children.add(
-                                ValueListenableBuilder<bool>(
-                                  valueListenable: manager.isLoading,
-                                  builder: (context, isLoading, child) {
-                                    return FilterChip(
-                                      selected: isSelected,
-                                      showCheckmark: !isLoading,
-                                      label: Text(time.time),
-                                      side: BorderSide(
-                                        color: isSelected ? context.colors.primary : context.colors.outlineVariant,
-                                      ),
-                                      avatar: isSelected && isLoading ? const LoadingIcon() : null,
-                                      onSelected:
-                                          isLoading
-                                              ? null
-                                              : (selected) {
-                                                if (!selected) return;
-                                                manager.setTemperatureTime(time.value);
-                                              },
-                                    );
-                                  },
-                                ),
-                              );
-                            }
+                                  children.add(
+                                    ValueListenableBuilder<bool>(
+                                      valueListenable: manager.isLoading,
+                                      builder: (context, isLoading, child) {
+                                        return FilterChip(
+                                          selected: isSelected,
+                                          showCheckmark: !isLoading,
+                                          label: Text(time.time),
+                                          side: BorderSide(
+                                            color: isSelected ? context.colors.primary : context.colors.outlineVariant,
+                                          ),
+                                          avatar: isSelected && isLoading ? const LoadingIcon() : null,
+                                          onSelected:
+                                              isLoading
+                                                  ? null
+                                                  : (selected) {
+                                                    if (!selected) return;
+                                                    manager.setTemperatureTime(time.value);
+                                                  },
+                                        );
+                                      },
+                                    ),
+                                  );
+                                }
 
-                            children.add(
-                              const Padding(
-                                padding: EdgeInsets.only(right: 8),
-                                child: VerticalDivider(width: 16, indent: 8, endIndent: 8),
-                              ),
+                                children.add(
+                                  const Padding(
+                                    padding: EdgeInsets.only(right: 8),
+                                    child: VerticalDivider(width: 16, indent: 8, endIndent: 8),
+                                  ),
+                                );
+
+                                return Row(mainAxisSize: MainAxisSize.min, spacing: 8, children: children);
+                              },
                             );
-
-                            return Row(mainAxisSize: MainAxisSize.min, spacing: 8, children: children);
                           },
-                        );
-                      },
-                    ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Row(
+                    spacing: 8,
+                    children: [
+                      const Icon(Symbols.thermostat_rounded, size: 24),
+                      Text('氣溫'.i18n, style: context.textTheme.titleMedium),
+                    ],
                   ),
-                ],
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Row(
-                spacing: 8,
-                children: [
-                  const Icon(Symbols.thermostat_rounded, size: 24),
-                  Text('氣溫'.i18n, style: context.textTheme.titleMedium),
-                ],
+                ),
+              ),
+            );
+          },
+        ),
+        Positioned(
+          top: 24 + 48 + 16,
+          left: 24,
+          child: SafeArea(
+            child: BlurredContainer(
+              elevation: 4,
+              shadowColor: context.colors.shadow.withValues(alpha: 0.4),
+              child: Selector<SettingsUserInterfaceModel, bool>(
+                selector: (context, model) => model.useFahrenheit,
+                builder: (context, useFahrenheit, child) {
+                  return ColorLegend(
+                    reverse: true,
+                    unit: useFahrenheit ? '℉' : '℃',
+                    appendUnit: true,
+                    items: [
+                      ColorLegendItem(color: const Color(0xff4d4e51), value: useFahrenheit ? -20.asFahrenheit : -20),
+                      ColorLegendItem(color: const Color(0xff0000ff), value: useFahrenheit ? -10.asFahrenheit : -10),
+                      ColorLegendItem(color: const Color(0xff6495ED), value: useFahrenheit ? 0.asFahrenheit : 0),
+                      ColorLegendItem(color: const Color(0xff95d07e), value: useFahrenheit ? 10.asFahrenheit : 10),
+                      ColorLegendItem(color: const Color(0xfff6e78b), value: useFahrenheit ? 20.asFahrenheit : 20),
+                      ColorLegendItem(color: const Color(0xffff4500), value: useFahrenheit ? 30.asFahrenheit : 30),
+                      ColorLegendItem(color: const Color(0xff8B0000), value: useFahrenheit ? 40.asFahrenheit : 40),
+                    ],
+                  );
+                },
               ),
             ),
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
