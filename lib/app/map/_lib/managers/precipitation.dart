5c310ed8bedbe67f7ceb933610263f2e723584ed
Kamiya <kamiya@kamiya.app>
2025-08-06 12:00:41 +0800

    feat: map legends

--- lib/app/map/_lib/managers/precipitation.dart
+++ lib/app/map/_lib/managers/precipitation.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/material.dart';
+
 import 'package:collection/collection.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+import 'package:provider/provider.dart';
+
 import 'package:dpip/api/exptech.dart';
 import 'package:dpip/api/model/weather/rain.dart';
 import 'package:dpip/app/map/_lib/manager.dart';
 import 'package:dpip/app/map/_lib/utils.dart';
+import 'package:dpip/app/map/_widgets/map_legend.dart';
 import 'package:dpip/core/i18n.dart';
 import 'package:dpip/core/providers.dart';
 import 'package:dpip/models/data.dart';
@@ -11,13 +18,10 @@ import 'package:dpip/utils/extensions/latlng.dart';
 import 'package:dpip/utils/extensions/string.dart';
 import 'package:dpip/utils/geojson.dart';
 import 'package:dpip/utils/log.dart';
+import 'package:dpip/widgets/blurred_container.dart';
 import 'package:dpip/widgets/map/map.dart';
 import 'package:dpip/widgets/sheet/morphing_sheet.dart';
 import 'package:dpip/widgets/ui/loading_icon.dart';
-import 'package:flutter/material.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
-import 'package:provider/provider.dart';
 
 class RainData {
   final double latitude;
@@ -314,95 +318,51 @@ class PrecipitationMapLayerSheet extends StatelessWidget {
       _ => interval,
     };
 
-    return MorphingSheet(
-      title: '降水'.i18n,
-      borderRadius: BorderRadius.circular(16),
-      elevation: 4,
-      partialBuilder: (context, controller, sheetController) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Selector<DpipDataModel, UnmodifiableListView<String>>(
-            selector: (context, model) => model.precipitation,
-            builder: (context, precipitation, header) {
-              final times = precipitation.map((time) {
-                final t = time.toSimpleDateTimeString(context).split(' ');
-                return (date: t[0], time: t[1], value: time);
-              });
-              final grouped = times.groupListsBy((time) => time.date).entries.toList();
+    return Stack(
+      children: [
+        MorphingSheet(
+          title: '降水'.i18n,
+          borderRadius: BorderRadius.circular(16),
+          elevation: 4,
+          partialBuilder: (context, controller, sheetController) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Selector<DpipDataModel, UnmodifiableListView<String>>(
+                selector: (context, model) => model.precipitation,
+                builder: (context, precipitation, header) {
+                  final times = precipitation.map((time) {
+                    final t = time.toSimpleDateTimeString(context).split(' ');
+                    return (date: t[0], time: t[1], value: time);
+                  });
+                  final grouped = times.groupListsBy((time) => time.date).entries.toList();
 
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  header!,
-                  SizedBox(
-                    height: kMinInteractiveDimension,
-                    child: ValueListenableBuilder<String?>(
-                      valueListenable: manager.currentPrecipitationInterval,
-                      builder: (context, currentPrecipitationInterval, child) {
-                        const intervals = PrecipitationMapLayerManager.precipitationIntervals;
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      header!,
+                      SizedBox(
+                        height: kMinInteractiveDimension,
+                        child: ValueListenableBuilder<String?>(
+                          valueListenable: manager.currentPrecipitationInterval,
+                          builder: (context, currentPrecipitationInterval, child) {
+                            const intervals = PrecipitationMapLayerManager.precipitationIntervals;
 
-                        return ListView.separated(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          scrollDirection: Axis.horizontal,
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          itemCount: intervals.length,
-                          itemBuilder: (context, index) {
-                            final interval = intervals[index];
-                            final isSelected = interval == currentPrecipitationInterval;
+                            return ListView.separated(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              scrollDirection: Axis.horizontal,
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              itemCount: intervals.length,
+                              itemBuilder: (context, index) {
+                                final interval = intervals[index];
+                                final isSelected = interval == currentPrecipitationInterval;
 
-                            return ValueListenableBuilder<bool>(
-                              valueListenable: manager.isLoading,
-                              builder: (context, isLoading, child) {
-                                return FilterChip(
-                                  selected: isSelected,
-                                  showCheckmark: !isLoading,
-                                  label: Text(getIntervalLabel(interval)),
-                                  side: BorderSide(
-                                    color: isSelected ? context.colors.primary : context.colors.outlineVariant,
-                                  ),
-                                  avatar: isSelected && isLoading ? const LoadingIcon() : null,
-                                  onSelected:
-                                      isLoading
-                                          ? null
-                                          : (selected) {
-                                            if (!selected) return;
-                                            manager.setPrecipitationInterval(interval);
-                                          },
-                                );
-                              },
-                            );
-                          },
-                          separatorBuilder: (context, index) => const SizedBox(width: 8),
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: kMinInteractiveDimension,
-                    child: ValueListenableBuilder<String?>(
-                      valueListenable: manager.currentPrecipitationTime,
-                      builder: (context, currentPrecipitationTime, child) {
-                        return ListView.builder(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          scrollDirection: Axis.horizontal,
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          itemCount: grouped.length,
-                          itemBuilder: (context, index) {
-                            final MapEntry(key: date, value: group) = grouped[index];
-
-                            final children = <Widget>[Text(date)];
-
-                            for (final time in group) {
-                              final isSelected = time.value == currentPrecipitationTime;
-
-                              children.add(
-                                ValueListenableBuilder<bool>(
+                                return ValueListenableBuilder<bool>(
                                   valueListenable: manager.isLoading,
                                   builder: (context, isLoading, child) {
                                     return FilterChip(
                                       selected: isSelected,
                                       showCheckmark: !isLoading,
-                                      label: Text(time.time),
+                                      label: Text(getIntervalLabel(interval)),
                                       side: BorderSide(
                                         color: isSelected ? context.colors.primary : context.colors.outlineVariant,
                                       ),
@@ -412,43 +372,116 @@ class PrecipitationMapLayerSheet extends StatelessWidget {
                                               ? null
                                               : (selected) {
                                                 if (!selected) return;
-                                                manager.setPrecipitationTime(time.value);
+                                                manager.setPrecipitationInterval(interval);
                                               },
                                     );
                                   },
-                                ),
-                              );
-                            }
-
-                            children.add(
-                              const Padding(
-                                padding: EdgeInsets.only(right: 8),
-                                child: VerticalDivider(width: 16, indent: 8, endIndent: 8),
-                              ),
+                                );
+                              },
+                              separatorBuilder: (context, index) => const SizedBox(width: 8),
                             );
-
-                            return Row(mainAxisSize: MainAxisSize.min, spacing: 8, children: children);
                           },
-                        );
-                      },
-                    ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: kMinInteractiveDimension,
+                        child: ValueListenableBuilder<String?>(
+                          valueListenable: manager.currentPrecipitationTime,
+                          builder: (context, currentPrecipitationTime, child) {
+                            return ListView.builder(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              scrollDirection: Axis.horizontal,
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              itemCount: grouped.length,
+                              itemBuilder: (context, index) {
+                                final MapEntry(key: date, value: group) = grouped[index];
+
+                                final children = <Widget>[Text(date)];
+
+                                for (final time in group) {
+                                  final isSelected = time.value == currentPrecipitationTime;
+
+                                  children.add(
+                                    ValueListenableBuilder<bool>(
+                                      valueListenable: manager.isLoading,
+                                      builder: (context, isLoading, child) {
+                                        return FilterChip(
+                                          selected: isSelected,
+                                          showCheckmark: !isLoading,
+                                          label: Text(time.time),
+                                          side: BorderSide(
+                                            color: isSelected ? context.colors.primary : context.colors.outlineVariant,
+                                          ),
+                                          avatar: isSelected && isLoading ? const LoadingIcon() : null,
+                                          onSelected:
+                                              isLoading
+                                                  ? null
+                                                  : (selected) {
+                                                    if (!selected) return;
+                                                    manager.setPrecipitationTime(time.value);
+                                                  },
+                                        );
+                                      },
+                                    ),
+                                  );
+                                }
+
+                                children.add(
+                                  const Padding(
+                                    padding: EdgeInsets.only(right: 8),
+                                    child: VerticalDivider(width: 16, indent: 8, endIndent: 8),
+                                  ),
+                                );
+
+                                return Row(mainAxisSize: MainAxisSize.min, spacing: 8, children: children);
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Row(
+                    spacing: 8,
+                    children: [
+                      const Icon(Symbols.water_drop_rounded, size: 24),
+                      Text('降水'.i18n, style: context.textTheme.titleMedium),
+                    ],
                   ),
-                ],
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Row(
-                spacing: 8,
-                children: [
-                  const Icon(Symbols.water_drop_rounded, size: 24),
-                  Text('降水'.i18n, style: context.textTheme.titleMedium),
+                ),
+              ),
+            );
+          },
+        ),
+        Positioned(
+          top: 24 + 48 + 16,
+          left: 24,
+          child: SafeArea(
+            child: BlurredContainer(
+              elevation: 4,
+              shadowColor: context.colors.shadow.withValues(alpha: 0.4),
+              child: ColorLegend(
+                unit: 'mm',
+                items: [
+                  ColorLegendItem(color: const Color(0xffc2c2c2), value: 0),
+                  ColorLegendItem(color: const Color(0xFF9CFCFF), value: 10),
+                  ColorLegendItem(color: const Color(0xFF059BFF), value: 30),
+                  ColorLegendItem(color: const Color(0xFF39FF03), value: 50),
+                  ColorLegendItem(color: const Color(0xFFFFFB03), value: 100),
+                  ColorLegendItem(color: const Color(0xFFFF9500), value: 200),
+                  ColorLegendItem(color: const Color(0xFFFF0000), value: 300),
+                  ColorLegendItem(color: const Color(0xFFFB00FF), value: 500),
+                  ColorLegendItem(color: const Color(0xFF960099), value: 1000),
+                  ColorLegendItem(color: const Color(0xFF000000), value: 2000),
                 ],
               ),
             ),
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
