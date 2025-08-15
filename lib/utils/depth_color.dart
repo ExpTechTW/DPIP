1a8c0bc59482ed2db0c1637b4ee28516f5256a19
lowrt <nineder197789@gmail.com>
2025-08-15 20:47:26 +0800

    fix: function name

--- lib/utils/depth_color.dart
+++ lib/utils/depth_color.dart
@@ -1,16 +1,16 @@
 import 'dart:ui';
 
-const depth0 = Color(0xFF000000);
-const depth1 = Color(0xFFFF0000);
-const depth2 = Color(0xFFFF6400);
-const depth3 = Color(0xFFFFC800);
-const depth4 = Color(0xFF00C800);
-const depth5 = Color(0xFF00C8C8);
-const depth6 = Color(0xFF0000C8);
+const kDepthColor0 = Color(0xFF000000);
+const kDepthColor1 = Color(0xFFFF0000);
+const kDepthColor2 = Color(0xFFFF6400);
+const kDepthColor3 = Color(0xFFFFC800);
+const kDepthColor4 = Color(0xFF00C800);
+const kDepthColor5 = Color(0xFF00C8C8);
+const kDepthColor6 = Color(0xFF0000C8);
 
-Color depth(double depth) {
+Color getDepthColor(double depth) {
   final depthList = [5, 15, 30, 50, 100, 150];
-  final colorList = [depth1, depth2, depth3, depth4, depth5, depth6];
+  final colorList = [kDepthColor1, kDepthColor2, kDepthColor3, kDepthColor4, kDepthColor5, kDepthColor6];
 
   if (depth <= depthList.first) return colorList.first;
 
@@ -22,5 +22,5 @@ Color depth(double depth) {
       return Color.lerp(colorList[i], colorList[i + 1], localT)!;
     }
   }
-  return depth0;
+  return kDepthColor0;
 }
