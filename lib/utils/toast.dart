468a42d12cf2e220b1dccda93b86ecda24a2010d
lowrt <nineder197789@gmail.com>
2025-11-09 16:33:50 +0800

    revert

--- lib/utils/toast.dart
+++ lib/utils/toast.dart
@@ -1,75 +1,29 @@
 import 'package:dpip/utils/extensions/build_context.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void showToast(BuildContext context, ToastWidget toast) {
-  final snackBar = SnackBar(
-    behavior: SnackBarBehavior.floating,
-    backgroundColor: Colors.transparent,
-    elevation: 0,
-    duration: const Duration(seconds: 3, milliseconds: 500),
-    content: AnimatedFade(child: Center(child: toast)),
+  final fToast = FToast();
+  fToast.init(context);
+  fToast.showToast(
+      child: toast,
+      toastDuration: const Duration(seconds: 3),
+      fadeDuration: Durations.short4,
+      gravity: ToastGravity.BOTTOM,
+      isDismissible: true,
   );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
-
-class AnimatedFade extends StatefulWidget {
-  final Widget child;
-  const AnimatedFade({super.key, required this.child});
-
-  @override
-  State<AnimatedFade> createState() => AnimatedFadeState();
-}
-
-class AnimatedFadeState extends State<AnimatedFade>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller = AnimationController(
-      vsync: this,
-      duration: Durations.short4,
-      reverseDuration: Durations.short4,
-  )..forward();
-
-  late final Animation<double> _opacity = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      if (mounted) _controller.reverse();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) => FadeTransition(opacity: _opacity, child: widget.child);
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 }
 
 class ToastWidget extends StatelessWidget {
   final List<Widget> children;
   const ToastWidget({super.key, required this.children});
 
-  factory ToastWidget.text(String text, {Key? key, Widget? icon}) {
-    return ToastWidget(
-      key: key,
-      children: [
+  ToastWidget.text(String text, {super.key, Widget? icon})
+      : children = [
         if (icon != null) icon,
         if (icon != null) const SizedBox(width: 4),
-        Flexible(
-          child: Builder(
-            builder: (context) => Text(
-              text,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+    Flexible(child: Text(text, textAlign: TextAlign.center)),
+  ];
 
   @override
   Widget build(BuildContext context) {
