f2da1a1d1cf4ce671199fd89236d8f403eba727d
BigDuck <ytriowten@gmail.com>
2026-04-11 22:25:59 +0800

    Update blurred_button.dart

--- lib/app/home/_widgets/blurred_button.dart
+++ lib/app/home/_widgets/blurred_button.dart
@@ -139,7 +139,9 @@ class BlurredIconButton extends StatelessWidget {
             style: IconButton.styleFrom(
               backgroundColor:
                   backgroundColor ?? context.colors.surfaceContainerHighest.withValues(alpha: 0.6),
-              foregroundColor: context.colors.outline,
+              foregroundColor: Theme.of(context).brightness == Brightness.dark
+                  ? const Color.fromARGB(255, 200, 200, 200)
+                  : const Color.fromARGB(255, 70, 70, 70),
             ),
             onPressed: onPressed,
             icon: icon,
